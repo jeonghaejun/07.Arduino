3af62999a553928ef343cc93f6cea42e4ec88c95
wjdgownsll <wjdgownsll@gmail.com>
2021-03-08 22:48:20 +0900

    21.03.08 2nd Arduino

--- chapter05/keypad/ex02/app.ino
+++ chapter05/keypad/ex02/app.ino
@@ -1,8 +1,7 @@
-#include <EEPROM.h>
 #include <Keypad.h>
 #include <SimpleTimer.h>
 #include <Servo.h>
-#include <Led.h>
+#include <DoorLock.h>
 
 const byte ROWS = 4; // 4행
 const byte COLS = 4; // 4열
@@ -26,34 +25,7 @@ int timer_id;
 
 Servo door;
 Led led(4); // 부저와 같이 동작
-
-String read_password()
-{
-    String password = "";
-    for (int i = 0; i < 20; i++)
-    {
-        char ch = EEPROM.read(i);
-        if (ch == '\0') // 문자열의 끝이면 중지
-        {
-            break;
-        }
-        password += ch;
-    }
-    return password;
-}
-
-void write_password(String password)
-{
-    int size = password.length();
-    for (int i = 0; i < size; i++)
-    {
-        char c = password[i];
-        Serial.println(c);
-        EEPROM.write(i, c);
-    }
-    EEPROM.write(size, '\0'); // 문자열의 끝 기록
-}
-
+DoorLock doorlock;
 // 키 입력을 하면 input에 구성
 // F를 누르면 완료...
 // 이때까지 입력한 문자열을
@@ -65,7 +37,7 @@ void setup()
     door.attach(5);
     door.write(0);
     led.off();
-    PASSWORD = read_password(); // 저장된 비밀번호 읽어오기
+    PASSWORD = doorlock.read_password(); // 저장된 비밀번호 읽어오기
 }
 
 void beep(int interval = 100)
@@ -130,7 +102,7 @@ void loop()
             if (input[0] == 'C')
             {
                 PASSWORD = input.substring(1); // substring(from,to) to는 미포함
-                write_password(PASSWORD);
+                doorlock.write_password(PASSWORD);
                 Serial.println("New Password: " + PASSWORD);
             }
             else
